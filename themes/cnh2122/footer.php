219934a255005eab3e5122dba38d9b23473739d9
JoseUTorres <73760867+JoseUTorres@users.noreply.github.com>
2021-07-27 17:53:53 -0700

    Footer

--- themes/cnh2122/footer.php
+++ themes/cnh2122/footer.php
@@ -1,5 +1,30 @@
 <!--  responsible for bottom section of the site -->
-<footer class="footer text-center py-2 theme-bg-dark">		   
-    <p class="copyright"><a href="https://youtube.com/FollowAndrew">FollowAndrew</a></p> 
-</footer>
+<footer>
+        <div class="row">
+            <div class="column">
+                
+                <div class="social-text">Check us out on social media!</div>
+                <div class="social-icons">
+                    <a href="https://facebook.com"><img src="facebook.png" alt="facebook"></a>
+                    <a href="https://instagram.com" style="padding-left: 94px;"><img src="instagram.png" alt="instagram"></a>
+                    <a href="https://twitter.com" style="padding-left: 94px;"><img src="twitter.png" alt="facebook"></a>
+                </div>
+                <div class="contact-questions">Questions?</div>
+                <div class="contact-box">
+                    <button class="contact-button">Contact us</button>
+                </div>
+                <div class="contact-ticket">Issue with the website? Outdated information?</div>
+                <div class="ticket-box">
+                    <button class="contact-button">Submit a ticket</button>
+                </div>
+                
+            </div>
+            <div class="column">
+                
+                <div class="kiwanis-logo"><img src="KSLP logo.png" alt="kiwanis-logo"></div>
+                <div class="copyright">© 2000 - 2021 California-Nevada-Hawaii District of Circle K International. All rights reserved.</div>
+
+            </div>
+        </div>
+    </footer>
    
