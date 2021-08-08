a6e6f09eda8035670b4e4d952804b83627cbe2ec
Renz Lane <krlane.cki@gmail.com>
2021-08-08 12:37:46 -0700

    added img assets; fixed footer styling

--- themes/cnh2122/footer.php
+++ themes/cnh2122/footer.php
@@ -2,12 +2,12 @@
 <footer>
         <div class="row">
             <div class="column">
-                
+    
                 <div class="social-text">Check us out on social media!</div>
                 <div class="social-icons">
-                    <a href="https://facebook.com"><img src="facebook.png" alt="facebook"></a>
-                    <a href="https://instagram.com" style="padding-left: 94px;"><img src="instagram.png" alt="instagram"></a>
-                    <a href="https://twitter.com" style="padding-left: 94px;"><img src="twitter.png" alt="facebook"></a>
+                    <a href="https://facebook.com"><img src="<?php bloginfo('template_url'); ?>/assets/images/facebook.png" alt="facebook"></a>
+                    <a href="https://instagram.com" style="padding-left: 94px;"><img src="<?php bloginfo('template_url'); ?>/assets/images/instagram.png" alt="instagram"></a>
+                    <a href="https://twitter.com" style="padding-left: 94px;"><img src="<?php bloginfo('template_url'); ?>/assets/images/twitter.png" alt="facebook"></a>
                 </div>
                 <div class="contact-questions">Questions?</div>
                 <div class="contact-box">
@@ -21,7 +21,7 @@
             </div>
             <div class="column">
                 
-                <div class="kiwanis-logo"><img src="KSLP logo.png" alt="kiwanis-logo"></div>
+                <div class="kiwanis-logo"><img src="<?php bloginfo('template_url'); ?>/assets/images/KSLP logo.png" alt="kiwanis-logo"></div>
                 <div class="copyright">© 2000 - 2021 California-Nevada-Hawaii District of Circle K International. All rights reserved.</div>
 
             </div>
