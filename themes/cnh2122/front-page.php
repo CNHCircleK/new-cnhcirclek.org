91966506d87b1970647a6b4dc68c968a77db3f61
Jose Torres <jose.uriel.torres5@gmail.com>
2021-11-08 13:41:54 -0800

    front page changes from me

--- themes/cnh2122/front-page.php
+++ themes/cnh2122/front-page.php
@@ -3,7 +3,7 @@
 		get_header();
 	?>
 	<!--Main Wrapper for the website-->
-    <div class="main-wrapper">
+	<div class="main-wrapper">
 		<!--Main column for the website aka the left col-->
 		<div class="main-col">
 			<!--Title and paragraph for front page-->
@@ -13,9 +13,9 @@
 				<div class="title">of Circle K International</div>
 			</div>
 
-			<div class="paragraph padding-left-standard">Our organization of more than 2,100 college students across California, Nevada, and Hawaii believes in enriching the world through community service - one member, one child, and one community at a time.</div>
+			<div class="paragraph padding-left-indent">Our organization of more than 2,100 college students across California, Nevada, and Hawaii believes in enriching the world through community service - one member, one child, and one community at a time.</div>
 			<!--Links container-->
-			<div class="front-page-links padding-left-standard">
+			<div class="front-page-links padding-left-indent">
 					<a href="about" class="page-link page-link-padding page-link-padding-right">
 						<div class="page-link-text">
 							About Us
@@ -31,25 +31,115 @@
 							<img src="<?php bloginfo('template_url'); ?>/assets/images/link-line.png" alt="link-line" class="page-link-border">
 						</div>
 					</a>
-				</div>
-				<!--Tenet Cards-->
-				<div class="sub-title padding-left-indent">Our Tenets</div>
-				<div class="tenet-card-container">
+			</div>
+			<!--Tenet Cards-->
+			<div class="sub-title padding-left-indent">Our Tenets</div>
+			<div class="tenet-card-container">
+				<!--Leadership Card-->
+				<a href="#" class="tenet-card-link">
 					<div class="tenet-card">
 						<div class="tenet-card-header-container">
 							Leadership
 							<img src="<?php bloginfo('template_url'); ?>/assets/images/smaller-vector.png" alt="vector" class="tenet-card-vector">
 							<img src="<?php bloginfo('template_url'); ?>/assets/images/smaller-link-line.png" alt="link-line">
 						</div>
-						<img src="<?php bloginfo('template_url'); ?>/assets/images/leadership.png" alt="leadership" class="">
+						<img src="<?php bloginfo('template_url'); ?>/assets/images/leadership.png" alt="leadership" class="tenet-card-image">
+					</div>
+				</a>
+				<div class="tenet-card-padding"></div>
+				<!--Service Card-->
+				<a href="#" class="tenet-card-link">
+					<div class="tenet-card">
+						<div class="tenet-card-header-container">
+							Service
+							<img src="<?php bloginfo('template_url'); ?>/assets/images/smaller-vector.png" alt="vector" class="tenet-card-vector">
+							<img src="<?php bloginfo('template_url'); ?>/assets/images/smaller-link-line.png" alt="link-line">
+						</div>
+						<img src="<?php bloginfo('template_url'); ?>/assets/images/service.png" alt="service" class="tenet-card-image">
+					</div>
+				</a>
+				<div class="tenet-card-padding"></div>
+				<!--Fellowship Card-->
+				<a href="#" class="tenet-card-link">
+					<div class="tenet-card">
+						<div class="tenet-card-header-container">
+							Fellowship
+							<img src="<?php bloginfo('template_url'); ?>/assets/images/smaller-vector.png" alt="vector" class="tenet-card-vector">
+							<img src="<?php bloginfo('template_url'); ?>/assets/images/smaller-link-line.png" alt="link-line">
+						</div>
+						<img src="<?php bloginfo('template_url'); ?>/assets/images/fellowship.png" alt="fellowship" class="tenet-card-image">
+					</div>
+				</a>
+			</div>
+			<!-- Organization Supports -->
+			<div class="sub-title padding-left-indent our-org-padding">Our Organization Supports...</div>
+			<div class="our-org-container">
+				<img src="<?php bloginfo('template_url'); ?>/assets/images/kfh.png" alt="kfh" class="our-org-image-padding">
+				<img src="<?php bloginfo('template_url'); ?>/assets/images/pediactric-trauma-program.png" alt="pediactric-trauma-program" class="our-org-image-padding">
+				<img src="<?php bloginfo('template_url'); ?>/assets/images/enviormental-defense-fund.png" alt="enviormental-defense-fund" class="our-org-image-padding">
+				<img src="<?php bloginfo('template_url'); ?>/assets/images/LDF.png" alt="LDF" class="our-org-image-padding">
+			</div>
+		<!-- End of Main Column -->
+		</div>
+		<!-- News -->
+		<div class="sub-title padding-left-indent news-padding">News</div>
+		<div class="news-container padding-left-indent">
+			<div class="news-box">
+				<div class="news-box-body-container">
+					<div class="news-title">CNH Stands in Solidarity with Black Lives Matter</div>
+					<div class="news-posted-by">June 4, 2020 | Matthew Kim</div>
+					<div class="news-body">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim, ornare tellus praesent sit nisi. Consequat dis nec aliquam nullam vel. Vulputate 
+						ipsum duis magna arcu ac at pellentesque nunc, sed. Enim amet cursus nisl eu. Purus risus felis, sapien mauris, et mauris pellentesque. 
+					</div>
+					<div class="news-read-more-container">
+						<button class="news-read-more">Read More</button>
 					</div>
 				</div>
-
 			</div>
-
+			<div class="news-box-padding"></div>
+			<div class="news-box">
+				<div class="news-box-body-container">
+					<div class="news-title">2020-2021 District Committee Appointments</div>
+					<div class="news-posted-by">June 4, 2020 | Matthew Kim</div>
+					<div class="news-body">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim, ornare tellus praesent sit nisi. Consequat dis nec aliquam nullam vel. Vulputate 
+						ipsum duis magna arcu ac at pellentesque nunc, sed. Enim amet cursus nisl eu. Purus risus felis, sapien mauris, et mauris pellentesque. 
+					</div>
+					<div class="news-read-more-container">
+						<button class="news-read-more">Read More</button>
+					</div>
+				</div>
+			</div>
 		</div>
-	
-    </div>
+		<!-- Featured Section -->
+		<div class="featured-section-container padding-left-indent">
+			<!-- Featured Event -->
+			<a href="#" class="featured-event-link">
+				<div class="featured-event-card">
+					<div class="featured-event-header-container">
+						Featured Discrict Event
+						<img src="<?php bloginfo('template_url'); ?>/assets/images/bigger-vector.png" alt="vector" class="featured-event-vector">
+						<img src="<?php bloginfo('template_url'); ?>/assets/images/bigger-link-line.png" alt="link-line">
+					</div>
+					<img src="<?php bloginfo('template_url'); ?>/assets/images/dcon-event-banner.png" alt="featured event" class="featured-event-image">
+				</div>
+			</a>
+			<div class="featured-event-card-padding"></div>
+			<!-- Sunspot -->
+			<a href="#" class="featured-event-link">
+				<div class="featured-event-card">
+					<div class="featured-event-header-container">
+						The Sunspot - Disctrict Blog
+						<img src="<?php bloginfo('template_url'); ?>/assets/images/bigger-vector.png" alt="vector" class="featured-event-vector">
+						<img src="<?php bloginfo('template_url'); ?>/assets/images/bigger-link-line.png" alt="link-line">
+					</div>
+					<img src="<?php bloginfo('template_url'); ?>/assets/images/the-sunspot.png" alt="featured event" class="featured-event-image">
+				</div>
+			</a>
+		</div>
+	<!-- End of Main Wrapper -->
+	</div>
     
 	<?php
 		get_footer();
